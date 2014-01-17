b0dc5de667e1a5a812156d69e778eafaa2a239fd
theminor <theminor@gmail.com>
2014-01-17 13:07:37 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var Presence = require('ninja-presence-base');
-var wifiLocation = require('wifi_location');
+// var wifiLocation = require('wifi_location');
 
 module.exports = Presence;
 
@@ -148,3 +148,36 @@ Presence.prototype.config = function(rpc,cb) {
       self._app.log.error('Unknown rpc method', rpc.method, rpc);
   }
 };
+
+/*
+var Presence = require('ninja-presence-base');
+var wifiLocation = require('wifi_location');
+
+module.exports = Presence;
+
+Presence.prototype.G = 'wifi';
+Presence.prototype.name = 'Presence - Wifi AP';
+Presence.prototype.V = 0;
+Presence.prototype.D = 261;
+
+Presence.prototype.scan = function() {
+  var self = this;
+
+  wifiLocation.wifiTowers(function(err, towers) {
+    if (err) {
+      self._app.log.warn('Failed to get Wifi access points', err);
+    } else {
+      towers.forEach(function(tower) {
+          self.see({
+                name: tower.ssid,
+                id: tower.mac_address,
+                distance: Math.abs(tower.signal_strength)
+          });
+      });
+    }
+
+    self.scanComplete();
+  });
+
+};
+*/
